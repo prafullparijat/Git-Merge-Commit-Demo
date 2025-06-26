8911d47245fda8896eeceb2e6dfca4e7491e64e4
Prafull <prafullparijat@gmail.com>
2025-06-26 19:28:07 +0000

    added addition method

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 38;
+        int b = 19;
+        int sum, diff, mul;
+        Main calc = new Main();
+        sum = calc.add(a, b);
+        System.out.println("Sum of A and B is "+sum);
+        //diff = calc.substract(a, b);
+        //System.out.println("Difference of A and B is "+ diff);
+        //mul = calc.multiply(a, b);
+        //System.out.println("Multiplication of A and B is "+ mul);
+    }
+
+    public int addition(int a, int b)
+    {
+        return a+b;
+    }
+
+}
+
